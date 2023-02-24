21a400df3cd9203f1c99e5ef9e52b61fc61c34c3
daniel107x <DMejiaV310796@outlook.com>
2023-02-23 20:46:13 -0600

    Changed java and spring version

--- backend/src/main/java/com/daniel107x/expenses/repository/ExpensesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.daniel107x.expenses.repository;
-
-import com.daniel107x.expenses.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpensesRepository extends JpaRepository<Expense, Long> {
-}
--- backend/src/main/java/com/daniel107x/expenses/repository/IncomesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.daniel107x.expenses.repository;
-
-import com.daniel107x.expenses.model.Income;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IncomesRepository extends JpaRepository<Income, Long> {
-}
