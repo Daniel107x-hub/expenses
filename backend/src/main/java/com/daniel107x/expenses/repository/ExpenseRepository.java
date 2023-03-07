8dd41f67332e52edfdc9df0fbbc0fb8b160b7a8e
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2023-03-07 13:17:24 -0600

    Added modifications to repositories

--- backend/src/main/java/com/daniel107x/expenses/repository/ExpenseRepository.java
+++ backend/src/main/java/com/daniel107x/expenses/repository/ExpenseRepository.java
@@ -2,6 +2,8 @@ package com.daniel107x.expenses.repository;
 
 import com.daniel107x.expenses.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 }
